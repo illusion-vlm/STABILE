47db092e5a8efeef67a00ddc7f666497fba1d7c6
illusion-vlm <144680499+illusion-vlm@users.noreply.github.com>
2026-03-25 11:51:06 +0800

    Delete lib directory

--- lib/logger.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-def logger(file_path=None, _str=''):
-    if file_path:
-        _str = str(datetime.datetime.now()) + ': ' + _str
-        f = open(file_path, 'a')
-        f.write(_str)
-        f.close()
-
